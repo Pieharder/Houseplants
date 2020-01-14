a32c24facf3b2a346e711648e3b40f2598dc05d9
geoff goetz and michelle morin <michelle@>
2020-01-14 09:51:22 -0800

    Create and add images, JS, CSS and initial html code.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".zz").click(function(){
+    $(".zz-initially-hidden").toggle();
+  })
+  $(".snake").click(function() {
+    $(".snake-initially-hidden").toggle();
+  })
+})
